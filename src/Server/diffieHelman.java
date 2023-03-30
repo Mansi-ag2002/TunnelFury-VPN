64aaf67e5ba7a65d660229306bb065a5e7258950
Mansi Agrawal <107387619+Mansi-ag2002@users.noreply.github.com>
2023-03-30 08:13:02 +0530

    Update diffieHelman.java

--- src/Server/diffieHelman.java
+++ src/Server/diffieHelman.java
@@ -1,56 +1,50 @@
 package project;
 
-import java.security.AlgorithmParameterGenerator;
-import java.security.AlgorithmParameters;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.KeyFactory;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.InvalidParameterSpecException;
-import java.security.spec.X509EncodedKeySpec;
-
-import javax.crypto.KeyAgreement;
-import javax.crypto.spec.DHParameterSpec;
-
-
-public class diffieHellmanHelper {
-
-public KeyAgreement keyAgreement;
-	
-	// Used source from http://docs.oracle.com/javase/1.4.2/docs/guide/security/jce/JCERefGuide.html to help with DHHandshake
-
-	public DHParameterSpec generateDiffieHellmanParameters() throws NoSuchAlgorithmException, InvalidParameterSpecException {
-		AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DH");
-        paramGen.init(512);
-        AlgorithmParameters params = paramGen.generateParameters();
-        return (DHParameterSpec) params.getParameterSpec(DHParameterSpec.class);
+public class diffieHelman {
+	private static long power(long a, long b, long p)
+	{
+	    if (b == 1)
+	        return a;
+	    else
+	        return (((long)Math.pow(a, b)) % p);
 	}
-
-	public KeyPair createKeyPair(DHParameterSpec diffieHellmanParameters) throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException {
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DH");
-        keyPairGenerator.initialize(diffieHellmanParameters);
-        KeyPair keyPair = keyPairGenerator.generateKeyPair();
-        setKeyAgreement(KeyAgreement.getInstance("DH"));
-        getKeyAgreement().init(keyPair.getPrivate());
-        return keyPair;
-	}
-
-	public PublicKey getUnencodedPublicKey(byte[] publicKeyEncoded) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, IllegalStateException {
-		KeyFactory keyFactory = KeyFactory.getInstance("DH");
-		X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(publicKeyEncoded);
-		return keyFactory.generatePublic(x509EncodedKeySpec);
-	}
-
-	public KeyAgreement getKeyAgreement() {
-		return keyAgreement;
-	}
-
-	public void setKeyAgreement(KeyAgreement keyAgreement) {
-		this.keyAgreement = keyAgreement;
+	public static void main(String[] args)
+	{
+	    long P, G, x, a, y, b, ka, kb;
+	     
+	    // Both the persons will be agreed upon the
+	    // public keys G and P
+	     
+	    // A prime number P is taken
+	    P = 23;
+	    System.out.println("The value of P:" + P);
+	     
+	    // A primitive root for P, G is taken
+	    G = 9;
+	    System.out.println("The value of G:" + G);
+	     
+	    // Alice will choose the private key a
+	    // a is the chosen private key
+	    a = 4;
+	    System.out.println("The private key a for Alice:" + a);
+	     
+	    // Gets the generated key
+	    x = power(G, a, P);
+	     
+	    // Bob will choose the private key b
+	    // b is the chosen private key   
+	    b = 3;
+	    System.out.println("The private key b for Bob:" + b);
+	     
+	    // Gets the generated key
+	    y = power(G, b, P);
+	     
+	    // Generating the secret key after the exchange
+	    // of keys
+	    ka = power(y, a, P); // Secret key for Alice
+	    kb = power(x, b, P); // Secret key for Bob
+	     
+	    System.out.println("Secret key for the Alice is:" + ka);
+	    System.out.println("Secret key for the Bob is:" + kb);
 	}
 }
-
