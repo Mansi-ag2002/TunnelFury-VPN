dce65e63660358ebcf05d02da77ef1d41a4b4eef
Mansi Agrawal <107387619+Mansi-ag2002@users.noreply.github.com>
2023-05-02 14:40:12 +0530

    Create CryptoInterface.java

---
+++ src/CryptoInterface.java
@@ -0,0 +1,11 @@
+package com;
+
+public interface CryptoInterface {
+	public boolean startServer( int port, String sharedKey ); //Start server and wait for client connections
+	public boolean startClient( String host, int port, String sharedKey ); //Start client and do handshake. Expected call GUIInterface.connectionReady()
+	public void endConnection(); //End connection
+	public void closeServer(); //shutdown server
+	public boolean sendMessage( String s ); //sends a message
+	public void acceptConnection();
+	public void receiveMessages();
+}
